acd0bede3c4154a73173d5a222345306776769f4
Peter Boothe <pboothe@google.com>
2018-07-10 13:38:56 -0400

    Put the fileinfo into github

---
+++ fileinfo/fileinfo.go
@@ -0,0 +1,18 @@
+package fileinfo
+
+import (
+	"os"
+
+	"github.com/m-lab/pusher/bytecount"
+)
+
+// A LocalDataFile holds all the information we require about a file.
+type LocalDataFile struct {
+	FullRelativeName string
+	Info             os.FileInfo
+	// Cache the size so sums of filesize are always using the same
+	// integer value, in case the file grows in size between receipt and
+	// upload.
+	CachedSize bytecount.ByteCount
+}
+
