8f41025f86479664c87e25347342755129568f54
Peter Boothe <pboothe@google.com>
2018-07-23 15:09:32 -0400

    Fix stale comment

--- finder/findfiles.go
+++ finder/findfiles.go
@@ -16,8 +16,6 @@ import (
 // FindFiles recursively searches through a given directory to find all the files which are old enough to be eligible for upload.
 // The list of files returned is sorted by mtime.
 func FindFiles(directory string, minFileAge time.Duration) ([]*tarcache.LocalDataFile, error) {
-	// Initially expect around 1024 files.  Chosen because it's a nice round number.
-	// TODO: Choose a better default.
 	eligibleFiles := make([]*tarcache.LocalDataFile, 0)
 	eligibleTime := time.Now().Add(-minFileAge)
 	totalEligibleSize := int64(0)
