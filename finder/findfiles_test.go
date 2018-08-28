942613bb59aca832e32a4920da5cc33bf61d2e55
Peter Boothe <pboothe@google.com>
2018-08-28 17:16:15 -0400

    100% test coverage of listener.
    No more ignoring errors in findfiles tests.

--- finder/findfiles_test.go
+++ finder/findfiles_test.go
@@ -7,30 +7,28 @@ import (
 	"testing"
 	"time"
 
+	r "github.com/m-lab/go/runtimeext"
 	"github.com/m-lab/pusher/finder"
-
 	"github.com/m-lab/pusher/tarcache"
 )
 
 func TestFindForever(t *testing.T) {
 	// Set up the files.
-	tempdir, _ := ioutil.TempDir("/tmp", "find_file_test")
+	tempdir, err := ioutil.TempDir("/tmp", "find_file_test")
 	defer os.RemoveAll(tempdir)
-	file1, _ := os.Create(tempdir + "/oldest_file")
-	_, _ = file1.WriteString("data\n")
-	_ = file1.Close()
+	r.Must(err, "Could not set up temp dir")
+	// Set up the files
+	r.Must(ioutil.WriteFile(tempdir+"/oldest_file", []byte("data\n"), 0644), "WriteFile failed")
 	newtime := time.Now().Add(time.Duration(-13) * time.Hour)
-	_ = os.Chtimes(tempdir+"/oldest_file", newtime, newtime)
-	file2, _ := os.Create(tempdir + "/next_oldest_file")
-	_, _ = file2.WriteString("moredata\n")
-	_ = file2.Close()
+	r.Must(os.Chtimes(tempdir+"/oldest_file", newtime, newtime), "Chtimes failed")
+	r.Must(ioutil.WriteFile(tempdir+"/next_oldest_file", []byte("moredata\n"), 0644), "WriteFile failed")
 	newtime = time.Now().Add(time.Duration(-12) * time.Hour)
-	_ = os.Chtimes(tempdir+"/next_oldest_file", newtime, newtime)
+	r.Must(os.Chtimes(tempdir+"/next_oldest_file", newtime, newtime), "Chtimes failed")
 	// Set up the receiver channel.
 	foundFiles := make(chan *tarcache.LocalDataFile)
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
-	go finder.FindForever(ctx, tempdir, time.Duration(6)*time.Hour, foundFiles, 1*time.Nanosecond)
+	go finder.FindForever(ctx, tempdir, time.Duration(6)*time.Hour, foundFiles, 1*time.Microsecond)
 	localfiles := []*tarcache.LocalDataFile{
 		<-foundFiles,
 		<-foundFiles,
@@ -49,8 +47,10 @@ func TestFindForever(t *testing.T) {
 func TestFindForeverNoDirExists(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
-	tempdir, _ := ioutil.TempDir("/tmp", "find_file_test")
+	tempdir, err := ioutil.TempDir("/tmp", "find_file_test")
 	defer os.RemoveAll(tempdir)
-	go finder.FindForever(ctx, "/tmp/dne", time.Duration(time.Minute), nil, time.Duration(time.Millisecond))
+	r.Must(err, "Could not set up temp dir")
+	go finder.FindForever(ctx, "/tmp/dne", time.Duration(time.Millisecond), nil, time.Duration(time.Millisecond))
 	time.Sleep(1 * time.Second)
+	// If the finder doesn't crash on a bad directory, then it's a success.
 }
