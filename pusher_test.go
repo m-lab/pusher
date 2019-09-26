d40888f07ed2bc32651f8b07a0ebf93034b48c37
Peter Boothe <pboothe@google.com>
2019-09-26 14:28:03 -0400

    Comment cleanup

--- pusher_test.go
+++ pusher_test.go
@@ -29,8 +29,6 @@ func Test_mlabNameToNodeName(t *testing.T) {
 			nodeName: "this-is-not-a-hostname",
 			wantErr:  true,
 		},
-
-		// TODO: Add test cases.
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
